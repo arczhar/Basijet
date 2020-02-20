1ef0f14e5e7eb1df5d0fea582e646c5d4d48604f
Jragadio31 <ragadio.joseph@wmsu.edu.ph>
2020-02-20 21:45:50 +0800

    Updated

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,102 @@
+@extends('layouts.app', ['class' => 'bg-default'])
+
+@section('content')
+    @include('layouts.headers.guest')
+
+    <div class="container mt--8 pb-5">
+        <!-- Table -->
+        <div class="row justify-content-center">
+            <div class="col-lg-6 col-md-8">
+                <div class="card bg-secondary shadow border-0">
+                    <div class="card-header bg-transparent">
+                        <div class="text-muted text-center mt-2"><h1 class="display-3">{{ __('Register') }}</small></div>
+                    </div>
+                    <div class="card-body px-lg-5">
+                        <div class="text-center text-muted mb-4">
+                            <small>{{ __('Kindly please provide the following details') }}</small>
+                        </div>
+                        <form role="form" method="POST" action="{{ route('register') }}">
+                            @csrf
+
+                            <div class="form-group{{ $errors->has('firstname') ? ' has-danger' : '' }}">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                                    </div>
+                                    <input class="form-control{{ $errors->has('firstname') ? ' is-invalid' : '' }}" placeholder="{{ __('First Name') }}" type="text" name="firstname" value="{{ old('firstname') }}" required autofocus>
+                                </div>
+                            <div class="form-group{{ $errors->has('lastname') ? ' has-danger' : '' }}">
+                                    <div class="input-group input-group-alternative mb-3">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text"><i class="ni ni-hat-3"></i></span>
+                                        </div>
+                                        <input class="form-control{{ $errors->has('lastname') ? ' is-invalid' : '' }}" placeholder="{{ __('Last Name') }}" type="text" name="lastname" value="{{ old('lastname') }}" required autofocus>
+                                    </div>
+                                @if ($errors->has('firstname'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('firstname') }}</strong>
+                                    </span>
+                                @endif
+                                @if ($errors->has('lastname'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('lastname') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
+                                <div class="input-group input-group-alternative mb-3">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-email-83"></i></span>
+                                    </div>
+                                    <input class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="{{ __('Email') }}" type="email" name="email" value="{{ old('email') }}" required>
+                                </div>
+                                @if ($errors->has('email'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    </div>
+                                    <input class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="{{ __('Password') }}" type="password" name="password" required>
+                                </div>
+                                @if ($errors->has('password'))
+                                    <span class="invalid-feedback" style="display: block;" role="alert">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                            <div class="form-group">
+                                <div class="input-group input-group-alternative">
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text"><i class="ni ni-lock-circle-open"></i></span>
+                                    </div>
+                                    <input class="form-control" placeholder="{{ __('Confirm Password') }}" type="password" name="password_confirmation" required>
+                                </div>
+                            </div>
+                            <div class="text-muted font-italic">
+                                <small>{{ __('password strength') }}: <span class="text-success font-weight-700">{{ __('strong') }}strong</span></small>
+                            </div>
+                            <div class="row my-4">
+                                <div class="col-12">
+                                    <div class="custom-control custom-control-alternative custom-checkbox">
+                                        <input class="custom-control-input" id="customCheckRegister" type="checkbox">
+                                        <label class="custom-control-label" for="customCheckRegister">
+                                            <span class="text-muted">{{ __('I agree with the') }} <a href="#!">{{ __('Privacy Policy') }}</a></span>
+                                        </label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary mt-4">{{ __('Create account') }}</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,70 @@
+@extends('layouts.app', ['class' => 'bg-default'])
+
+@section('content')
+    <div class="header bg-gradient-primary py-7 py-lg-8">
+        <div class="container">
+            
+        <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <div class="mask flex-center">
+                  <div class="container">
+                    <div class="row align-items-center">
+                      <div class="col-md-7 col-12 order-md-1 order-2">
+                        <h4>Request service anywhere</h4>
+                        <p>Request service to fix your office airconditioning unit, refrigirator, water dispenser and more right on your computer or mobile phone.</p>
+                        </div>
+                      <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/I1Bpr0O.png" class="mx-auto" alt="slide"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="mask flex-center">
+                  <div class="container">
+                    <div class="row align-items-center">
+                      <div class="col-md-7 col-12 order-md-1 order-2">
+                        <h4>Stay Updated</h4>
+                        <p>SRSS sends you text updates on the status of your request.</p>
+                        </div>
+                      <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/L1pGpR9.png" class="mx-auto" alt="slide"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="carousel-item">
+                <div class="mask flex-center">
+                  <div class="container">
+                    <div class="row align-items-center">
+                      <div class="col-md-7 col-12 order-md-1 order-2">
+                        <h4>Great Customer Service</h4>
+                        <p>Property Management Office ensures that you are entitled to open dispute when you didn't receive your unit or it malfunctioned after service.</p>
+                         </div>
+                      <div class="col-md-5 col-12 order-md-2 order-1"><img src="https://i.imgur.com/tSCe2Kt.png" class="mx-auto" alt="slide"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            
+        </div>
+          <!--slide end--> 
+        <div class="separator separator-bottom separator-skew zindex-100">
+            <svg x="0" y="0" viewBox="0 0 2560 100" preserveAspectRatio="none" version="1.1" xmlns="http://www.w3.org/2000/svg">
+                <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+            </svg>
+        </div>
+            {{-- <div class="header-body text-center mt-7 mb-7">
+                <div class="row justify-content-center">
+                    <div class="col-lg-5 col-md-6">
+                        <h1 class="text-white">{{ __('Welcome!') }}</h1>
+                    </div>
+                </div>
+            </div> --}}
+        </div>
+        <a class="carousel-control-prev" href="#myCarousel" role="button" data-slide="prev"> <span class="carousel-control-prev-icon" aria-hidden="true"></span> <span class="sr-only">Previous</span> </a> <a class="carousel-control-next" href="#myCarousel" role="button" data-slide="next"> <span class="carousel-control-next-icon" aria-hidden="true"></span> <span class="sr-only">Next</span> </a>
+
+    </div>
+
+    <div class="container mt--10 pb-5"></div>
+@endsection
