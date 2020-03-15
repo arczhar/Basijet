da406ff6fc6abbda194a1d5a9352e3481f1404ed
Jhet <arczharjhet17@gmail.com>
2020-03-15 21:55:30 +0800

    adjust send

--- resources/views/users/sendrequest.blade.php
+++ resources/views/users/sendrequest.blade.php
@@ -1,10 +1,8 @@
 @extends('layouts.app')
-
 @section('content')
-    @include('layouts.headers.cards')
-    
+    @include('layouts.headers.cards')   
     <div class="container-fluid mt--7">
-      
+
     @if(Auth::user()->type== 'Client')
             <div class="row">
                 <div class="col-12">
@@ -31,7 +29,7 @@
                                     </div>
                                   @endif
                                 </div>
-                                <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Add Request</button>
+                                
                                 <div class="modal fade bd-example-modal-lg" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                                     <div class="modal-dialog modal-lg">
                                         <div class="modal-content">
@@ -41,143 +39,129 @@
                                                 <span aria-hidden="true">&times;</span>
                                                 </button>
                                             </div>
-                                          <form method="POST" action="{{ route('request.store') }}" >  
+                                            <form method="POST" action="{{ route('request.store') }}" >  
                                              @csrf
-                                            <div class="modal-body">
-                                                <form>
-                                                    <div class="row">
-                                                        <div class="col-md-6">
-                                                          <div class="form-group">
-                                                            
-                                                            <input type="date" class="form-control"  name="date" required>
-                                                          </div>
+                                                <div class="modal-body">
+                                                    <form>
+                                                        <div class="row">
+                                                            <div class="col-md-6">
+                                                              <div class="form-group">
+                                                                
+                                                                <input type="date" class="form-control"  name="date" required>
+                                                              </div>
+                                                            </div>
+                                                            <div class="col-md-6">
+                                                              <div class="form-group">
+                                                                <input type="text" class="form-control"  placeholder="RM NO."  name="rmno" required>
+                                                              </div>
+                                                            </div>
                                                         </div>
-                                                        <div class="col-md-6">
-                                                          <div class="form-group">
-                                                            <input type="text" class="form-control"  placeholder="RM NO."  name="rmno" required>
-                                                          </div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="row">
-                                                        <div class="col-md-6">
-                                                          <div class="form-group">
-                                                              <div class="input-group mb-4">
-                                                                <div class="input-group-prepend">
-                                                                    <span class="input-group-text"><i class=""></i></span>
+                                                        <div class="row">
+                                                            <div class="col-md-6">
+                                                              <div class="form-group">
+                                                                  <div class="input-group mb-4">
+                                                                    <div class="input-group-prepend">
+                                                                        <span class="input-group-text"><i class=""></i></span>
+                                                                    </div>
+                                                                      <input type="text" class="form-control" placeholder="College"  name="college"required>
+                                                                  </div>
+                                                              </div>
+                                                            </div>
+                                                            <div class="col-md-6">
+                                                              <div class="form-group">
+                                                                <div class="input-group mb-4">
+                                                                    <input type="text" class="form-control"  placeholder="Equipment/Unit/Model" name="equipment"required>
+                                                                  <div class="input-group-append">
+                                                                    <span class="input-group-text"><i class="#"></i></span>
+                                                                  </div>
                                                                 </div>
-                                                                  <input type="text" class="form-control" placeholder="Office"  name="college"required>
                                                               </div>
-                                                          </div>
+                                                            </div>
                                                         </div>
-                                                        <div class="col-md-6">
-                                                          <div class="form-group">
-                                                            <div class="input-group mb-4">
-                                                                <input type="text" class="form-control"  placeholder="Equipment/Unit/Model" name="equipment"required>
-                                                              <div class="input-group-append">
-                                                                <span class="input-group-text"><i class="#"></i></span>
-                                                              </div>
+                                                        <div class="row">
+                                                          <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <input type="text" class="form-control"  placeholder="Property Number" name="propertynumber"required>
+                                                            </div>
+                                                          </div>
+                                                          <div class="col-md-6">
+                                                            <div class="form-group">
+                                                                <input type="number" class="form-control"  placeholder="Quantity/Unit" min="1" max="10" name="quantity"required>
                                                             </div>
                                                           </div>
                                                         </div>
-                                                    </div>
-                                                    <div class="row">
-                                                      <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <input type="text" class="form-control"  placeholder="Property Number" name="propertynumber"required>
-                                                        </div>
+                                                        <h3 class="modal-title" id="exampleModalLabel">Nature of Request</h3>
+                                                        <div class="row">
+                                                            <div class="col-md-8"> 
+                                                              <input type="radio" name="service" value="repair" checked> Repair
+                                                              <input type="radio" name="service" value="cleaning" checked> Cleaning
+                                                              <input type="radio" name="service" value="wiring" checked> Wiring
+                                                              <br>
+                                                              <label for="others">Others</label>
+                                                            </div>
+                                                          </div>
+                                                        <br>
+                                                        <div class="modal-footer">                         
+                                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                                            <button type="submit" class="btn btn-primary">Send Request</button>                                 
                                                       </div>
-                                                      <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <input type="number" class="form-control"  placeholder="Quantity/Unit" min="1" max="10" name="quantity"required>
-                                                        </div>
-                                                      </div>
-                                                    </div>
-                                                    <h3 class="modal-title" id="exampleModalLabel">Nature of Request</h3>
-                                                    <div class="row">
-                                                        <div class="col-md-8"> 
-                                                          <input type="radio" name="service" value="Repair" > Repair
-                                                          <input type="radio" name="service" value="Cleaning" > Cleaning
-                                                          <input type="radio" name="service" value="Wiring" > Wiring
-                                                          <input type="radio" name="service" value="Tech Support" > Technical Support
-                                                          <br>
-                                                          
-                                                         
-                                                      </div>
-                                                    <br>
-                                                    <div class="modal-footer">
-                                                    
-                                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                                                    <button type="submit" class="btn btn-primary">Send Request</button>
-                                                  </div>
-                                      </form>
-                                            </div>
-                                  </form>
-                                            
+                                                    </form>
+                                                </div>
+                                            </form>                                            
                                         </div>
                                     </div>
-                                </div>
-                            </div>         
+                                </div>                            
+                                {{--table--}}                                      
+                                            <div class="card">
+                                              <div class="card-header border-0">
+                                                <div class="row align-items-center">
+                                                  <div class="col">
+                                                    <h3 class="mb-0">My Request</h3>
+                                                  </div>
+                                                  <div class="col text-right">
+                                                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target=".bd-example-modal-lg">Add Request</button>
+                                                  </div>
+                                                </div>
+                                              </div>
+                                              <div class="table-responsive">
+                                                <!-- Projects table -->
+                                                <div class="table-responsive">
+                                                  <table class="table align-items-center table-flush">
+                                                    <thead class="thead-light">
+                                                      <tr>
+                                                        <th scope="col" class="sort" data-sort="name">Date</th>
+                                                        <th scope="col" class="sort" data-sort="property">RM NO</th>
+                                                        <th scope="col" class="sort" data-sort="college">College</th>
+                                                        <th scope="col" class="sort" data-sort="equipment">Equipment/Unit/Model</th>
+                                                        <th scope="col" class="sort" data-sort="property">Property No.</th>
+                                                        <th scope="col" class="sort" data-sort="quantity">Quantity</th>
+                                                        <th scope="col" class="sort" data-sort="request">Nature of Request</th>
+                                                        <th scope="col" class="sort" data-sort="request">Status</th>
+                                                      </tr> 
+                                                    </thead>
+                                                    <tbody class="list">
+                                                      @foreach ($request as $row)
+                                                        <tr>
+                                                          <td>{{ $row->date}}</td>
+                                                          <td>{{ $row->rmno}}</td>                
+                                                          <td>{{ $row->college}}</td>
+                                                          <td>{{ $row->equipment}}</td>
+                                                          <td>{{ $row->propertynumber}}</td>
+                                                          <td>{{ $row->quantity}}</td>
+                                                          <td>{{ $row->service}}</td>
+                                                          <td>{{ $row->status}}</td>
+                                                        </tr>  
+                                                       @endforeach           
+                                                    </tbody>
+                                                  </table>
+                                                </div>
+                                              </div>
+                                            </div>
                         </div>
-                      </div>
-                </div>
-        @elseif(Auth::user()->type== 'Admin')
-        <div class="card">
-          <div class="card-header border-0">
-            <div class="row align-items-center">
-              <div class="col">
-                <h3 class="mb-0">Requests</h3>
-              </div>
-              
-            </div>
-          </div>
-          <div class="table-responsive">
-            <!-- Projects table -->
-            <div class="table-responsive">
-              <table class="table align-items-center table-flush">
-                <thead class="thead-light">
-                  <tr>
-                    <th scope="col">{{ __('Service Type')}}</th>
-                    <th scope="col">{{ __('Equipment')}}</th>
-                    <th scope="col">{{ __('Property Number')}}</th>
-                    <th scope="col">{{ __('Office')}}</th>
-                    <th scope="col">{{ __('Quantity')}}</th>
-                    <th scope="col">{{ __('Date')}}</th>
-                    <th scope="col">{{ __('Status')}}</th>
-                    <th scope="col"></th>
-                  </tr> 
-                </thead>
-                <tbody class="list">
-                  @foreach ($requests as $request)
-                  <tr>
-                    <td>{{ $request->service }}</td>
-                    <td>{{ $request->equipment}}</td>
-                    <td>{{ $request->propertynumber}}</td>
-                    <td>{{ $request->college}}</td>
-                    <td>{{ $request->quantity}}</td>
-                    <td>{{ $request->date}}</td>
-                    <td>{{ $request->status}}</td>
-                    <td class="text-right">
-                      <div class="dropdown">
-                          <a class="btn btn-sm btn-icon-only text-light" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                              <i class="fas fa-ellipsis-v"></i>
-                          </a>
-                  @endforeach 
-                  </tr>
-                </tbody>
-              </table>
-            </div>
-            <div class="card-footer py-4">
-              <nav class = "d-flex justify-content-end" aria-label="...">
-                {{$requests->links()}}
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      @endif
-    
-    </div>
-    
+                    </div>                      
+                </div>                          
+        @endif
+        
     </div>
         @include('layouts.footers.auth')
     </div>  
